e0acaaf5780f72ff3e580cca2c53980471533a78
鄭育承 <king4sam990816@gmail.com>
2019-03-03 14:25:47 +0800

    react dev tool install global hook

--- shells/webextension/src/GlobalHook.js
+++ shells/webextension/src/GlobalHook.js
@@ -14,4 +14,33 @@
 // Inject a `__REACT_DEVTOOLS_GLOBAL_HOOK__` global so that React can detect that the
 // devtools are installed (and skip its suggestion to install the devtools).
 
+var installGlobalHook = require('./installGlobalHook');
+
+var detectReact = `
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.on('renderer', function(evt) {
+  window.postMessage({
+    source: 'react-devtools-detector',
+    reactBuildType: evt.reactBuildType,
+  }, '*');
+});
+`;
+var saveNativeValues = `
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.nativeObjectCreate = Object.create;
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.nativeMap = Map;
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.nativeWeakMap = WeakMap;
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.nativeSet = Set;
+`;
+
+var js = (
+  ';(' + installGlobalHook.toString() + '(window))' +
+  saveNativeValues +
+  detectReact
+);
+
+// This script runs before the <head> element is created, so we add the script
+// to <html> instead.
+var script = document.createElement('script');
+script.textContent = js;
+document.documentElement.appendChild(script);
+script.parentNode.removeChild(script);
 console.log('extension!!');
