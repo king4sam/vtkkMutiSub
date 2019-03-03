44bc4276ca0dfa10f12c6ca65bd551230aaa0ce2
鄭育承 <king4sam990816@gmail.com>
2019-03-03 23:33:56 +0800

    message handler: fetch vtt

--- shells/webextension/src/contentScript.js
+++ shells/webextension/src/contentScript.js
@@ -2,6 +2,31 @@
 'use strict';
 
 console.info('contentscript!!');
+window._mutiSubs = new Map();
+
+window.addEventListener('message', function(receivedMessage) {
+  if (receivedMessage.data.source === 'subUrl') {
+    const lang = receivedMessage.data.lang;
+    console.log('got message from suburl', receivedMessage);
+    console.log('fetching... ' + receivedMessage.data.lang);
+
+    function getVtt(response) {
+      const vtt = response.text();
+      return vtt;
+    }
+
+    function addSubs(vtt) {
+      if (!window._mutiSubs.has(lang)) {
+        window._mutiSubs.set(lang, vtt);
+      }
+    }
+
+    fetch(receivedMessage.data.url)
+    .then(getVtt)
+    .then(addSubs);
+  }
+});
+
 var getSubUrl = require('./getSubUrl');
 
 var js = (
@@ -16,4 +41,3 @@ script.textContent = js;
 document.documentElement.appendChild(script);
 script.parentNode.removeChild(script);
 console.log('contentscript!!');
-
