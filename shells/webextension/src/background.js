1ec76469ed3dd85e6c13ffc87040150d7d3857a9
鄭育承 <king4sam990816@gmail.com>
2019-03-08 00:58:11 +0800

    change browser action to page action

--- shells/webextension/src/background.js
+++ shells/webextension/src/background.js
@@ -1,1 +1,10 @@
-console.log('background');
+// handle page action icon
+chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostEquals: 'https://www.kktv.me' },
+    }),
+    ],
+    actions: [new chrome.declarativeContent.ShowPageAction()],
+  }]);
+});
