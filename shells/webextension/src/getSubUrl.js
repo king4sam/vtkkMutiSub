d27ba78b8f328e087d6c84617d4968eac31dd4c7
鄭育承 <king4sam990816@gmail.com>
2019-03-17 23:26:32 +0800

    add original LICENSE

--- shells/webextension/src/getSubUrl.js
+++ shells/webextension/src/getSubUrl.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This function code of mountFiber() is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
 'use strict';
 
 function getSubUrl() {
